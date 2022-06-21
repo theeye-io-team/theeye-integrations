fa073365c46d5b8c85bf06f1b061481f62cea8fa
facugon <facugon@theeye.io>
2022-06-21 18:31:09 -0300

    change script structure

--- freshdesk_to_gchat/index.js
+++ freshdesk_to_gchat/index.js
@@ -1,9 +1,56 @@
+// error and output handlers must go first.
+
+/**
+ * @param {Object}
+ * @prop {Mixed} data
+ * @prop {Array} components
+ * @prop {Object} next
+ */
+const successOutput = ({ data, components, next }) => {
+  // https://documentation.theeye.io/core-concepts/scripts/#passing-arguments-in-workflow
+  const output = {
+    state: "success",
+    data,
+    components, // https://documentation.theeye.io/core-concepts/tasks/script_type/#components
+    next
+  }
+  console.log( JSON.stringify(output) )
+  process.exit(0)
+}
+
+/**
+ * @param {Error} err
+ */
+const failureOutput = (err) => {
+  console.error(err)
+  const output = {
+    state: "failure",
+    data: {
+      message: err.message,
+      code: err.code,
+      data: err.data
+    }
+  }
+  console.error( JSON.stringify(output) )
+  process.exit(1)
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(reason, 'Unhandled Rejection at Promise', p)
+  failureOutput(reason)
+})
+
+process.on('uncaughtException', err => {
+  console.error(err, 'Uncaught Exception thrown')
+  failureOutput(err)
+})
+
 
 const https = require('https')
 const URL = require('url')
 
 // NodeJs boilerplate
-const main = module.exports = async () => {
+const main = async () => {
   const payload = JSON.parse(process.argv[2])
 
   console.log(payload)
@@ -46,4 +93,9 @@ const main = module.exports = async () => {
   })
 
   return { data: value }
+}
+
+module.exports = () => {
+  // invoke main and capture result output
+  main().then(successOutput).catch(failureOutput)
 }
