1ff27ec1a671884dbde716a71388e3e7f461a9c6
damian-theeye <damian@theeye.io>
2022-07-05 09:49:29 -0300

    fix: promise array is not a function

--- feriados-service/index.js
+++ feriados-service/index.js
@@ -48,7 +48,7 @@ const checkForChanges = async (feriados, customer, access_token) => {
   } else {
     throw new Error('Multiple files with the same name...')
   }
-
+  return
   throw new Error('No changes')
 }
 
@@ -74,7 +74,7 @@ const processCustomers = (feriados) => {
           })
       )
     } else {
-      promiseArray({ customer, message: 'disabled' })
+      promiseArray.push({ customer, message: 'disabled' })
     }
   }
 
