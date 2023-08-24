b6515cebe9a999318a6949793bf8764587851271
Damian Porchietto <damian@theeye.io>
2023-08-24 12:07:48 -0300

    feat: image service

---
+++ image-service/index.js
@@ -0,0 +1,34 @@
+const vision = require('./lib/vision')
+
+const main = async (base64, {regexp}) => {
+    const [annotation]= await vision(base64)
+
+    const result = {
+        annotation,
+        fullText: annotation.fullTextAnnotation.text
+    }
+
+    if(regexp) {
+        const fullText = result.annotation.fullTextAnnotation.text
+        
+        const pattern = regexp.pattern || ''
+        const options = regexp.options || ''
+
+        result.valid = new RegExp(pattern, options).test(fullText)
+    }
+
+    return result
+}
+
+if(require.main === module) {
+    const base64 = ''
+    
+    const opt = {
+        regexp: {
+            pattern: '^\d{6}$',
+            options: 'gi'
+        }
+    }
+
+    main(base64, opt).then(console.log).catch(console.error)
+}
---
+++ image-service/lib/vision.js
@@ -0,0 +1,26 @@
+// use GOOGLE_APPLICATION_CREDENTIALS to load service account
+const config = require('../config/config.json')
+process.env.GOOGLE_APPLICATION_CREDENTIALS??=config.service_account.path
+
+const Vision = require('@google-cloud/vision')
+const vision = new Vision.ImageAnnotatorClient()
+
+const main = module.exports = async (base64) => {
+
+    console.log(`processing file`)
+
+    const request = {
+        image: { content: base64 },
+        features: [{
+            type: "DOCUMENT_TEXT_DETECTION",
+            model: (process.env.GOOGLE_VISION_MODEL || null)
+        }]
+    }
+
+    return await vision.annotateImage(request)
+}
+
+if (require.main === module) {
+    const base64 = ''
+    main(process.argv[2] || base64).then(console.log).catch(console.error)
+}
