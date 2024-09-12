f3c1b6a7a35d20e61d1c465efc1c0e5c91382e5b
Damian Porchietto <damian@theeye.io>
2024-09-12 11:07:41 -0300

    fix: feriados service

--- feriados-service/libs/webbot.js
+++ feriados-service/libs/webbot.js
@@ -3,25 +3,26 @@ const baseUrl = process.env.FERIADOS_WEBBOT_BASEURL
 const { DateTime } = require('luxon')
 const { GCHAT } = require('./google-chat')
 
-if(!process.env.FERIADOS_WEBBOT_BASEURL) throw new Error('Env FERIADOS_WEBBOT_BASEURL not defined.')
+if (!process.env.FERIADOS_WEBBOT_BASEURL) throw new Error('Env FERIADOS_WEBBOT_BASEURL not defined.')
+
 
 const browserOptions = {
-    headless: true,
+    headless: false,
     handleSIGINT: true,
     handleSIGTERM: true,
     handleSIGHUP: true,
     // executablePath: '/bin/google-chrome',
     args: [
-      '--no-sandbox',
-      '--disable-features=site-per-process',
-      '--disable-gpu',
-      '--window-size=1920,1024'
+        '--no-sandbox',
+        '--disable-features=site-per-process',
+        '--disable-gpu',
+        '--window-size=1920,1024'
     ]
 }
 
 const main = module.exports = async (year) => {
-if (!year) {
-    year = DateTime.now().toFormat('yyyy')
+    if (!year) {
+        year = DateTime.now().toFormat('yyyy')
     }
 
     const url = `${baseUrl}${year}`
@@ -31,47 +32,65 @@ if (!year) {
     await page.goto(url)
 
     try {
-        return await page.evaluate((year)=> {
-            extractNumbers = (str) => {
+        const payload = await page.evaluate((year) => {
+            const getMonth = (month) => {
+                const months = [
+                    'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
+                ]
+
+                const index = months.findIndex((m) => m === month.toLowerCase()) + 1
+
+                if(index < 0) throw new Error('wrong month definition, check ids')
+
+                return index
+            }
+
+            const extractNumbers = (str) => {
                 const regex = /^([0-9]*(\s|,)?)*./
                 const matches = str.match(regex)
-                const numbers = matches[0].replace('.','').split(',')
+                const numbers = matches[0].replace('.', '').split(',')
                 return numbers.map(Number)
             }
-    
+
             const eles = []
-    
+
             const calendarContainer = document.getElementById('calendar-container')
-            if(!calendarContainer) throw new Error('cannot find calendar container')
+            if (!calendarContainer) throw new Error('cannot find calendar container')
 
             const holidaysDiv = calendarContainer.querySelectorAll('div.holidays')
-            // if(holidaysDiv.length !== 12) throw new Error('holidays monthly div differes from expected value')
 
-            holidaysDiv.forEach( (ccc, index) => { 
-                ccc.querySelectorAll('p').forEach(p => {
-                const text = p.innerText
-                if (/\([a|b|c]\)$/.test(text) === false) {
-                    if(text) {
-                        const days = extractNumbers(text)
-                        console.log(text, days)
-                        for(const day of days) {
-                            if(day !== 0) {
-                                const month = String(index + 1)
-                                eles.push(`${String(day).length === 1 ? `0${day}` : day}-${month.length === 1 ? `0${month}` : month}-${year}`)        
+            for (const div of holidaysDiv) {
+                const parentElement = div.parentElement
+                const textMonth = parentElement.id.substring(1)
+                const holidays = div.querySelectorAll('p')
+
+                for (const p of holidays) {
+                    const text = p.innerText
+                    if (/\([a|b|c]\)$/.test(text) === false) {
+                        if (text) {
+                            const days = extractNumbers(text)
+                            for (const day of days) {
+                                if (day !== 0) {
+                                    const month = getMonth(textMonth)
+                                    eles.push(`${String(day).length === 1 ? `0${day}` : day}-${month.length === 1 ? `0${month}` : month}-${year}`)
+                                }
                             }
                         }
                     }
                 }
-                })
-            })
+
+            }
+
             return eles
         }, year)
-    } catch(err) {
-        await new GCHAT().sendMessage(JSON.stringify({message: err.message, stack: err.stack},null,2))
+
+        return payload
+    } catch (err) {
+        await new GCHAT().sendMessage(JSON.stringify({ message: err.message, stack: err.stack }, null, 2))
         throw err
     }
 }
 
-if(require.main === module) {
+if (require.main === module) {
     main().then(console.log).catch(console.error)
 }
